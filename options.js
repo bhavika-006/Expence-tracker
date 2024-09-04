db244cecda2d8d5f8fa024d9c2019d319c34f3ae
bhavika-006 <00bhavikaish@gmail.com>
2024-09-05 00:09:28 +0530

    Add files via upload

---
+++ options.js
@@ -0,0 +1,30 @@
+$(function(){
+
+    chrome.storage.sync.get('limit',function(budget){
+        $('#limit').val(budget.limit);
+    });
+
+    $('#saveLimit').click(function(){
+        var limit = $('#limit').val();
+        if (limit){
+            chrome.storage.sync.set({'limit': limit}, function(){
+                close();
+            });
+        }
+    });
+
+    $('#resetTotal').click(function(){
+        chrome.storage.sync.set({'total': 0}, function(){
+          
+            var notifOptions = {
+                type: "basic",
+                iconUrl: "icon48.png",
+                title: "Resetting Total",
+                message: "Expenses is set to be zero:0"
+            };
+           
+            chrome.notifications.create('resetNotif', notifOptions);
+           
+        });
+    });
+});
